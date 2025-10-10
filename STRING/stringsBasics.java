75fa7bbcd8b21376e2aab7481be4adaa6c41b09b
SumitSingh008 <sumitsingh0216@gmail.com>
2025-10-10 12:42:13 +0530

    string

---
+++ STRING/stringsBasics.java
@@ -0,0 +1,42 @@
+package STRING;
+
+import java.util.*;
+
+public class stringsBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // String s = "Sumti Kumar Singh";
+        // String su = "umit Kumar Singh";
+        // System.out.println(s);
+
+        // // taking input = next() and nextLine()
+        // // Function is String
+        // // charAt() = kisi bhi perticular index ke element ko fetch kr lega
+        // // charAt() ko normaly char ch = charAt() krke use krte h
+        // // length() = length pta krne ke liye s.length() string me hum array me
+        // s.length
+        // // se hi pta kr lete the
+
+        // // indexOf() and compareTo()
+
+        // // indexOf me agr me koi character de dunga to wo mujhe uska index de dega
+        // for
+        // // example
+
+        // System.out.println(s.indexOf('k'));
+
+        // String a = "Hello";
+        // String b = "Dello";
+        // System.out.println(a.compareTo(b));
+
+        // contains() and startsWith()
+        String str = "sumit control";
+        System.out.println(str.contains("con"));
+        System.out.println(str.startsWith("su")); // aisa hi ek function h endsWith
+
+        // String + int/char/string - javascript ke jaise h
+
+    }
+
+}
